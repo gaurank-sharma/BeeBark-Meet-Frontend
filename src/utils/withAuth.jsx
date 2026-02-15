56f9f1d95a1cf0edf4f698020f3acefdd2e98769
Gaurank <sharmagaurank63@gmail.com>
2026-02-16 01:08:19 +0530

    change

--- src/utils/withAuth.jsx
+++ src/utils/withAuth.jsx
@@ -1,3 +1,32 @@
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const withAuth = (WrappedComponent) => {
+//   const AuthComponent = (props) => {
+//     const router = useNavigate();
+
+//     const isAuthenticated = () => {
+//       if (localStorage.getItem("token")) {
+//         return true;
+//       }
+//       return false;
+//     };
+
+//     useEffect(() => {
+//       if (!isAuthenticated()) {
+//         router("/auth");
+//       }
+//     }, [router]);
+
+//     return <WrappedComponent {...props} />;
+//   };
+
+//   return AuthComponent;
+// };
+
+// export default withAuth;
+
+
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -5,15 +34,9 @@ const withAuth = (WrappedComponent) => {
   const AuthComponent = (props) => {
     const router = useNavigate();
 
-    const isAuthenticated = () => {
-      if (localStorage.getItem("token")) {
-        return true;
-      }
-      return false;
-    };
-
     useEffect(() => {
-      if (!isAuthenticated()) {
+      // Check directly inside the effect to avoid dependency issues
+      if (!localStorage.getItem("token")) {
         router("/auth");
       }
     }, [router]);
